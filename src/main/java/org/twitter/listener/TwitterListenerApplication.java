617191fb3f3fdd38d94baaac467aaafe17a100d3
Serhii Makhov <dead0896@gmail.com>
2018-10-23 20:53:28 +0300

    SPring boot project

---
+++ src/main/java/org/twitter/listener/TwitterListenerApplication.java
@@ -0,0 +1,12 @@
+package org.twitter.listener.twitterlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterListenerApplication.class, args);
+	}
+}
