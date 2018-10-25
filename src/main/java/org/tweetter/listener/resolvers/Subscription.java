7726011c815fe54efd206801c3678babfb237db3
Serhii Makhov <sergiy.makhov@onior.com>
2018-10-25 10:22:58 +0300

    Working subscription which sends current local time

--- src/main/java/org/tweetter/listener/resolvers/Subscription.java
+++ src/main/java/org/tweetter/listener/resolvers/Subscription.java
@@ -2,12 +2,16 @@ package org.tweetter.listener.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
 import graphql.schema.DataFetchingEnvironment;
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
 
 @Component
 public class Subscription implements GraphQLSubscriptionResolver {
 
-    public Tweet tweets(DataFetchingEnvironment environment) {
-        return new Tweet("Hello");
+    public Publisher<Tweet> tweets(DataFetchingEnvironment environment) {
+        return Flux.create(fluxSink -> fluxSink.next(new Tweet(LocalDateTime.now().toString())));
     }
 }
